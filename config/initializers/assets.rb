7e731080213b19cd279d5b50ed2dba23e8d4c11f
Akinori Fukumura <fukumura.akinori@gmail.com>
2016-12-14 16:59:49 +0000

    minor fix for rubocop

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( fullcalendar.3.1.0.js )
-Rails.application.config.assets.precompile += %w( moment.2.10.6.ja.js )
-Rails.application.config.assets.precompile += %w( moment.2.10.6.js )
-Rails.application.config.assets.precompile += %w( jquery.3.1.1.js )
+Rails.application.config.assets.precompile += %w(fullcalendar.3.1.0.js)
+Rails.application.config.assets.precompile += %w(moment.2.10.6.ja.js)
+Rails.application.config.assets.precompile += %w(moment.2.10.6.js)
+Rails.application.config.assets.precompile += %w(jquery.3.1.1.js)
