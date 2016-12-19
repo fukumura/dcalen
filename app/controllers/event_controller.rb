183494606751126fa4ac5c2773c0a732d834bda5
Akinori Fukumura <fukumura.akinori@gmail.com>
2016-12-19 16:13:45 +0000

    [WIP]eventsテーブルのレコードを登録

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,7 +1,25 @@
 class EventController < ApplicationController
-  def events
+  def show
     @event = Event.all
     # render :json => @event
+    respond_to do |format|
+      format.json {
+        render json:
+        @event.to_json(
+          only: [:title, :start, :end]
+        )
+      }
+    end
+  end
+
+  def create
+    event = Event.new
+    event.attributes = {
+      title: params[:title],
+      start: params[:start],
+      end: params[:end],
+    }
+    event.save
     respond_to do |format|
       format.json {
         render json:
