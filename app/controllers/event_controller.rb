6000f0c85963fbb3ef0b35f2d83eda5a136dceed
Akinori Fukumura <fukumura.akinori@gmail.com>
2016-12-27 03:45:33 +0000

    add event->available

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,7 +1,24 @@
 class EventController < ApplicationController
   def show
-    @event = Event.all
-    # render :json => @event
+    @event = Event.is_available
+    respond_to do |format|
+      format.json {
+        render json:
+        @event.to_json(
+          only: [:id, :title, :start, :end, :allDay]
+        )
+      }
+    end
+  end
+
+  def create
+    event = Event.new
+    event.attributes = {
+      title: params[:title],
+      start_at: params[:start_at],
+      finish_at: params[:finish_at],
+    }
+    event.save
     respond_to do |format|
       format.json {
         render json:
@@ -12,37 +29,19 @@ class EventController < ApplicationController
     end
   end
 
-  def create
-    event = Event.new
-    event.attributes = {
-      title: params[:title],
-      start: params[:start],
-      end: params[:end],
-    }
-    event.save
-    respond_to do |format|
-      format.json {
-        render json:
-        @event.to_json(
-          only: [:title, :start, :end]
-        )
-      }
-    end
-  end
-
   def update
     @event = Event.find(params[:id])
     @event.attributes = {
       title: params[:title],
-      start: params[:start],
-      end: params[:end],
+      start_at: params[:start_at],
+      finish_at: params[:finish_at],
     }
     @event.save
     respond_to do |format|
       format.json {
         render json:
         @event.to_json(
-          only: [:title, :start, :end]
+          only: [:id, :title, :start, :end]
         )
       }
     end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Event do
+
+  describe "Event be valid" do
+    it { expect(build(:event)).to be_valid }
+  end
+
   specify "titleは必須" do
     event = build(:event, :blunk_title)
     expect(event).not_to be_valid
     expect(event.errors[:title]).to be_present
   end
+
 end
