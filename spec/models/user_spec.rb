7d46d2c2dd71f2554f0c4ac35feaeb04bb1f390e
fukumura <fukumura.akinori@gmail.com>
2016-12-31 00:06:42 +0900

    Revert "add_test"

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "User be valid" do
-    it { expect(build(:user)).to be_valid }
-  end
-  specify "nameは必須" do
-    user = build(:user, :blunk_name)
-    expect(user).not_to be_valid
-    expect(user.errors[:name]).to be_present
-  end
-  specify "emailは必須" do
-    user = build(:user, :blunk_email)
-    expect(user).not_to be_valid
-    expect(user.errors[:email]).to be_present
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
